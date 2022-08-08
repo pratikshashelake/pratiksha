2afa14b5b0fd79dd934c56f22bdbb219870873c8
pratikshashelake <110670658+pratikshashelake@users.noreply.github.com>
2022-08-08 12:14:44 +0530

    Add files via upload

---
+++ Ca.java
@@ -0,0 +1,17 @@
+Public class Ca {
+int a=10;
+	public void m1()
+	{
+		int a=30;
+		System.out.println(a);
+		System.out.println(this.a);
+		System.out.println("M1 of Ca");
+	}
+	public void m2()
+{
+	System.out.println("M2 of Ca");	
+		
+	System.out.println("Main method");	
+		
+}
+}
